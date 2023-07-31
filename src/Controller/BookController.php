0ddc277390496e03c683578130d7473334b4e394
Jonathan Guerpillon <jonathanguerpillon@gmail.com>
2023-07-31 22:24:04 +0200

    add function update in BookManager et adjustement in other function

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -16,7 +16,6 @@ class BookController extends AbstractController
     #[Route('/', name: 'app_book_get_all', methods:['GET'])]
     public function get_all(BookRepository $bookRepository): JsonResponse
     {
-        $response = new JsonResponse();
         $books = $bookRepository->findAll();
         $booksArray = [];
         foreach ($books as $key => $book) {
@@ -30,13 +29,12 @@ class BookController extends AbstractController
             "data" => [
                 "books" => $booksArray
             ]
-        ], 200, []);
+        ], 200);
     }
 
     #[Route('/{id}', name: 'app_book_get_by_id', methods:['GET'])]
     public function get_by_id($id, BookRepository $bookRepository): JsonResponse
     {
-        $response = new JsonResponse();
         if (!filter_var($id, FILTER_VALIDATE_INT)) {
             return new JsonResponse([
                 "succes" => false,
@@ -60,14 +58,14 @@ class BookController extends AbstractController
                     "publication_year" => $book->getPublicationYear()
                 ]
             ]
-        ], 200, []);
+        ], 200);
     }
 
     #[Route('/', name: 'app_book_add', methods:['POST'])]
     public function add(Request $request, BookRepository $bookRepository): JsonResponse
     {
         $request_decode = json_decode($request->getContent());
-        if (is_string($request_decode->title) && strlen($request_decode->title) > 0 && strlen($request_decode->title) < 255 && is_string($request_decode->author) && strlen($request_decode->author) > 0 && strlen($request_decode->author) < 255 && is_int($request_decode->publication_year) && $request_decode->publication_year > 0 && $request_decode->publication_year <= date("Y")) {
+        if (is_string($request_decode->title) && strlen($request_decode->title) < 1 && strlen($request_decode->title) < 255 && is_string($request_decode->author) && strlen($request_decode->author) < 1 && strlen($request_decode->author) < 255 && is_int($request_decode->publication_year) && $request_decode->publication_year > 0 && $request_decode->publication_year <= date("Y")) {
             $book = new Book();
             $book->setTitle($request_decode->title);
             $book->setAuthor($request_decode->author);
@@ -90,5 +88,69 @@ class BookController extends AbstractController
             "succes" => false,
             "error" => "Bad Request"
         ], 400);
+    }
+
+    #[Route('/{id}', name: 'app_book_update', methods:['Put'])]
+    public function update($id, Request $request, BookRepository $bookRepository): JsonResponse
+    {
+        $request_decode = json_decode($request->getContent());
+        if (!filter_var($id, FILTER_VALIDATE_INT) || (empty($request_decode->title) && empty($request_decode->author) && empty($request_decode->publication_year))) {
+            return new JsonResponse([
+                "succes" => false,
+                "error" => "Bad Request"
+            ], 400);
+        }
+        if (isset($request_decode->title)) {
+            if (!is_string($request_decode->title) || strlen($request_decode->title) < 1 || strlen($request_decode->title) > 255) {
+                return new JsonResponse([
+                    "succes" => false,
+                    "error" => "Bad Request"
+                ], 400);
+            }
+        }
+        if (isset($request_decode->author)) {
+            if (!is_string($request_decode->author) || strlen($request_decode->author) < 1 || strlen($request_decode->author) > 255) {
+                return new JsonResponse([
+                    "succes" => false,
+                    "error" => "Bad Request"
+                ], 400);
+            }
+        }
+        if (isset($request_decode->publication_year)) {
+            if (!is_int($request_decode->publication_year) || $request_decode->publication_year < 1 || $request_decode->publication_year > intval(date("Y"))) {
+                return new JsonResponse([
+                    "succes" => false,
+                    "error" => "Bad Request"
+                ], 400);
+            }
+        }
+        $book = $bookRepository->findOneById($id);
+        if ($book == null) {
+            return new JsonResponse([
+                "succes" => false,
+                "error" => "The book was not found"
+            ], 404);
+        }
+        if (isset($request_decode->title)) {
+            $book->setTitle($request_decode->title);
+        }
+        if (isset($request_decode->author)) {
+            $book->setAuthor($request_decode->author);
+        }
+        if (isset($request_decode->publication_year)) {
+            $book->setPublicationYear($request_decode->publication_year);
+        }
+        $bookRepository->save($book, true);
+        return new JsonResponse([
+            "succes" => true,
+            "data" => [
+                "book" => [
+                    "id" => $book->getId(),
+                    "title" => $book->getTitle(),
+                    "author" => $book->getAuthor(),
+                    "publication_year" => $book->getPublicationYear()
+                ]
+            ]
+        ], 200);
     }
 }
